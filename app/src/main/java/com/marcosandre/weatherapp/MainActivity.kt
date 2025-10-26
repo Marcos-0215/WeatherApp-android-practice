7610612cbc91b11108f5dac4ec6a7fb14f68fae1
André <m.andre0215@gmail.com>
2025-10-26 11:20:58 -0300

    FEITA Pratica 01: Parte 2

--- app/src/main/java/com/marcosandre/weatherapp/MainActivity.kt
+++ app/src/main/java/com/marcosandre/weatherapp/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.marcosandre.weatherapp
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.marcosandre.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +39,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Andre",
+                    HomePage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +48,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+/*
+Na MainActivity, crie uma tela simples chamada HomePage contendo apenas uma
+mensagem de texto de boas-vindas e um botão para sair.
+Adapte o código usado na LoginPage como base.
+Ajuste os elementos da UI para ficar visualmente agradáveis.
+*/
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
+fun HomePage(modifier: Modifier = Modifier) {
+    val activity = LocalContext.current as Activity
+
+    Column(
+        modifier = modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo(a) ao WeatherApp!",
+            fontSize = 24.sp
+        )
+
+        Spacer(modifier = modifier.size(24.dp))
+
+        Button( onClick = {
+            Toast.makeText(activity, "Saiu com sucesso!", Toast.LENGTH_LONG).show()
+            activity.finish()
+        }
+        ) {
+            Text("Sair")
+        }
+
     }
 }
