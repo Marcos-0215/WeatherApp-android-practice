cb819b14f3272610683166256f8a5c6d64d70106
André <m.andre0215@gmail.com>
2025-11-09 12:38:52 -0300

    Pratica 03: Parte 1 (Criando uma lista simples)

---
+++ app/src/main/java/com/marcosandre/weatherapp/model/City.kt
@@ -0,0 +1,8 @@
+package com.marcosandre.weatherapp.model
+
+data class City(
+    val name : String,
+    val weather: String? = null,
+    val location: String? = null
+
+)
--- app/src/main/java/ui/ListPage.kt
+++ app/src/main/java/ui/ListPage.kt
@@ -1,17 +1,28 @@
 package ui
 
 import android.app.Activity
+import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -22,6 +33,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.marcosandre.weatherapp.model.City
+
 
 @Preview(showBackground = true)
 @Composable
@@ -42,5 +55,66 @@ fun ListPage(modifier: Modifier = Modifier) {
             fontSize = 20.sp
         )
 
+    }
+
+    val cityList = remember { getCities().toMutableStateList() }  // faz a lista ser reativa
+    val activity = LocalContext.current as Activity // Para os Toasts
+    LazyColumn ( // cria a lista vertical
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        items(cityList, key = { it.name }) { city ->   // percorre cityList e exibe um CityItem para cada cidade
+            CityItem(city = city, onClose = {
+                Toast.makeText(activity, "Fechou ${city.name}", Toast.LENGTH_LONG).show()
+
+            }, onClick = {
+                Toast.makeText(activity, "Clicou em ${city.name}", Toast.LENGTH_LONG).show()
+            })
+        }
+    }
+
+
+}
+
+// Gera uma lista fake de cidades, útil pra testar
+private fun getCities() = List(20) { i ->
+    City(name = "Cidade $i", weather = "Carregando clima...")
+}
+
+
+@Composable
+fun CityItem(
+    city: City,                     // A cidade que será exibida
+    onClick: () -> Unit,            // O que fazer a clicar no item
+    onClose: () -> Unit,            // O que fazer ao clicar no botão X
+    modifier: Modifier = Modifier   // permite aplicar modificações (padding, alinhamento, etc)
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Ícone à esquerda
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
+        )
+        Spacer(modifier = Modifier.size(12.dp))
+
+        // Nome e Clima
+        Column(modifier = modifier.weight(1f)) {
+            Text(modifier = Modifier,
+                text = city.name,
+                fontSize = 24.sp)
+            Text(modifier = Modifier,
+                text = city.weather?:"Carregando clima...",
+                fontSize = 16.sp)
+
+        }
+
+        // Botão X para remover item
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
     }
 }
