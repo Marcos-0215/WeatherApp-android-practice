9d9eaf3dfcd9af92df541bb590f37dd609a984ea
André <m.andre0215@gmail.com>
2025-11-15 20:34:39 -0300

    Pratica 04: Parte 3 (Adicionando e Testando o Mapa)

--- app/src/main/java/ui/MapPage.kt
+++ app/src/main/java/ui/MapPage.kt
@@ -22,6 +22,12 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.marcosandre.weatherapp.viewmodel.MainViewModel
 
 @Preview(showBackground = true)
@@ -29,21 +35,50 @@ import com.marcosandre.weatherapp.viewmodel.MainViewModel
 fun MapPage(modifier: Modifier = Modifier,
             viewModel: MainViewModel
 ) {
+    val recife = LatLng(-8.05, -34.9)
+    val caruaru = LatLng(-8.27, -35.98)
+    val joaopessoa = LatLng(-7.12, -34.84)
+    val camPosState = rememberCameraPositionState ()
 
-    Column(
-        modifier = modifier.fillMaxSize()
-            .background(Color.Gray)
-            .wrapContentSize(Alignment.Center)
+    GoogleMap (modifier = Modifier.fillMaxSize(),
+            onMapClick = {
+                viewModel.add(
+                    "Cidade@${it.latitude}:${it.longitude}",
+                    location = it
+                )
+            },
+            cameraPositionState = camPosState
+        ) {
 
-    ) {
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+        Marker (
+            state = MarkerState(position = recife),
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
         )
 
+        Marker (
+            state = MarkerState(position = caruaru),
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)
+        )
+
+        Marker (
+            state = MarkerState(position = joaopessoa),
+            title = "João Pessoa",
+            snippet = "Marcador em João Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+        )
+
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                Marker( state = MarkerState(position = it.location),
+                    title = it.name, snippet = "${it.location}"
+                )
+            }
+        }
+
     }
+
 }
