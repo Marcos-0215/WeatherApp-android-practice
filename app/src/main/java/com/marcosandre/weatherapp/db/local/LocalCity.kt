de768046392042625717e5a1a178777d971b0a01
André <m.andre0215@gmail.com>
2026-02-01 13:33:19 -0300

    [Prática 11: Parte 2] Criando os componentes do Room

---
+++ app/src/main/java/com/marcosandre/weatherapp/db/local/LocalCity.kt
@@ -0,0 +1,30 @@
+package com.marcosandre.weatherapp.db.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.android.gms.maps.model.LatLng
+import com.marcosandre.weatherapp.model.City
+
+@Entity
+data class LocalCity(
+    @PrimaryKey
+    var name: String,
+    var latitude: Double,
+    var longitude: Double,
+    var isMonitored: Boolean
+)
+
+
+fun LocalCity.toCity() = City(
+    name = this.name,
+    location = LatLng(this.latitude, this.longitude),
+    isMonitored = this.isMonitored
+)
+
+
+fun City.toLocalCity() = LocalCity(
+    name = this.name,
+    latitude = this.location?.latitude ?: 0.0,
+    longitude = this.location?.longitude ?: 0.0,
+    isMonitored = this.isMonitored
+)
